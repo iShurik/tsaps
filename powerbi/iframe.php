30f1585687131d3541819960d02867f79f265391
ishurik <lexkuzin@gmail.com>
2024-03-14 13:44:28 +0300

    editing powerbi iframe

--- powerbi/iframe.php
+++ powerbi/iframe.php
@@ -2,6 +2,8 @@
 
 $data = file_get_contents('https://app.powerbi.com/view?r=eyJrIjoiOWM0NTYwNjgtMjQwNy00ODc4LWEyNTYtZTk0ZDFiNTlmYWMzIiwidCI6ImY3OWQwNDBlLWM0ZTgtNDE4Mi1hMjg2LTg4YmUyNmM4MzIzMiIsImMiOjEwfQ%3D%3D');
 
-echo $data;
+
 
 ?>
+
+<iframe src="https://app.powerbi.com/view?r=eyJrIjoiOWM0NTYwNjgtMjQwNy00ODc4LWEyNTYtZTk0ZDFiNTlmYWMzIiwidCI6ImY3OWQwNDBlLWM0ZTgtNDE4Mi1hMjg2LTg4YmUyNmM4MzIzMiIsImMiOjEwfQ%3D%3D"></frame>
