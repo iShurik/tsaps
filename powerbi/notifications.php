b7f6c2b1e96a65a5e0c870cca5eb016b21adc92e
ishurik <lexkuzin@gmail.com>
2024-03-14 09:38:25 +0300

    adding pbi notifications

---
+++ powerbi/notifications.php
@@ -0,0 +1,37 @@
+<?php
+
+$tgbotKey = '7163787842:AAFYdx7bwjkSBXG3j_E_h3ygYTZXu4fKwOc';
+$update = file_get_contents('php://input');
+$data = json_decode($update, true);
+
+$chatId = '413750305';
+$message = $data['message'];
+
+$ch = curl_init();
+
+define('TOKEN', $tgbotKey);
+
+function sendTelegram($method, $response)
+{
+	$ch = curl_init('https://api.telegram.org/bot' .TOKEN. '/' . $method);  
+	curl_setopt($ch, CURLOPT_POST, 1);  
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	$res = curl_exec($ch);
+	curl_close($ch);
+ 
+	return $res;
+}
+
+	sendTelegram(
+		'sendMessage', 
+		array(
+			'chat_id' => $chatId,
+			'text' => $update,
+			'parse_mode' => 'HTML'
+		)
+	);  
+
+echo $update;
+?>
