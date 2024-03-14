ba81303a84922371d1d6180426b2def7df6beeac
ishurik <lexkuzin@gmail.com>
2024-03-14 13:24:29 +0300

    editing powerbi iframe

---
+++ powerbi/iframe.php
@@ -0,0 +1,7 @@
+<?php
+
+$data = file_get_contents('https://app.powerbi.com/view?r=eyJrIjoiOWM0NTYwNjgtMjQwNy00ODc4LWEyNTYtZTk0ZDFiNTlmYWMzIiwidCI6ImY3OWQwNDBlLWM0ZTgtNDE4Mi1hMjg2LTg4YmUyNmM4MzIzMiIsImMiOjEwfQ%3D%3D');
+
+echo $data;
+
+?>
